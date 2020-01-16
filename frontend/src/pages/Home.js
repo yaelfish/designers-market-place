9c8248de948d8ccf019572bfc0f77907eaaec885
Crystalk107 <30338496+Crystalk107@users.noreply.github.com>
2020-01-16 06:37:35 +0200

    some hompage updates

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import coverImg from '../assets/images/cover2.jpg'
 
 export default class Home extends Component {
 
   render() {
     return (
       <div className="home">
-         
+        <div className="coverImg">  </div>
+    
        
      </div>
     );
