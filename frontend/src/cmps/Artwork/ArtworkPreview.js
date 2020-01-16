1ed46022b97eec3970157b7fe3a5fcdd3163f9b5
unknown <ohadavidar@gmail.com>
2020-01-16 13:35:25 +0200

    styling the Artwork Preview

--- frontend/src/cmps/Artwork/ArtworkPreview.js
+++ frontend/src/cmps/Artwork/ArtworkPreview.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import bin from '../../assets/images/icons/bin.png';
-import { removeArtwork } from '../../actions/ArtworkActions';
+// import bin from '../../assets/images/icons/bin.png';
+import beforeLike from '../../assets/images/icons/before-like.png';
+// import afterLike from '../../assets/images/icons/after-like.png';
+// import bin from '../../assets/images/icons/bin.png';
+// import { removeArtwork } from '../../actions/ArtworkActions';
 
 
 export default class ArtworkPreview extends Component {
@@ -13,11 +16,13 @@ export default class ArtworkPreview extends Component {
             <Link to={`/artwork/${_id}`}>
                 <div className="card-artwork flex justify-center align-center column">
                     <img src={imgUrl} alt={name} />
-                    <div className="preview-info">
-                        <p className="preview-artist">{artist.fullName}</p>
-                        <p className="preview-price">price: {price}$</p>
-                        {/* <div><img src={bin} onClick={}></img></div> */}
+                    <div className="preview-info flex align-center justify-space-between">
+                            <p className="preview-artwork">{name}</p>
+                            {/* <img className="preview-icon" src={bin} onClick={}></img>*/}
+                            <img className="preview-icon" src={beforeLike}></img>
+                            {/* <img className="preview-icon" src={afterLike}></img> */}
                     </div>
+                    <div className="preview-price none">price: {price}$</div>
                 </div>
             </Link>
         )
