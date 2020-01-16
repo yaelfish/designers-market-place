e216cd7b423fce891b26d2c95ed9813d112d9196
Crystalk107 <30338496+Crystalk107@users.noreply.github.com>
2020-01-16 12:35:28 +0200

    nav changes

--- frontend/src/cmps/MainNavbar.js
+++ frontend/src/cmps/MainNavbar.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 
 export default function NavBar(props) {
-    return <nav className="main-nav">
-        <ul>
-            <li><NavLink to='/' activeClassName="active-link" exact>Home</NavLink></li>
-            <li><NavLink to='/about' activeClassName="active-link" exact>About</NavLink></li>
-            <li><NavLink to='/artwork' activeClassName="active-link" exact>Artworks</NavLink></li>
+    
+    return (   
+    <nav className={props.isHome ? "main-nav absolute" : "main-nav"}>
+        <ul className="nav-links"> 
+            <li ><NavLink className="nav-link" to='/' activeClassName="active-link" exact>Home</NavLink></li>
+            <li ><NavLink className="nav-link" to='/about' activeClassName="active-link" exact>About</NavLink></li>
+            <li ><NavLink className="nav-link" to='/artwork' activeClassName="active-link" exact>Artworks</NavLink></li>
         </ul>
     </nav>
+    )
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import coverImg from '../assets/images/cover2.jpg'
+import MainNavbar from '../cmps/MainNavbar';
 
 export default class Home extends Component {
+  state = {
+    isHome: true
+  }
 
   render() {
     return (
+      <React.Fragment>
+      <MainNavbar isHome={this.state.isHome} />
       <div className="home">
-        <div className="coverImg"></div>
+        <div className="coverImg flex justify-center align-center">Artwork Gallery</div>
      </div>
+     </React.Fragment>
     );
   }
 }
