90ecaf85fd32282f42c7407433b8f44d644d0890
Crystalk107 <30338496+Crystalk107@users.noreply.github.com>
2020-01-16 15:23:05 +0200

    homepage+navbar changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,16 +7,24 @@ export default class Home extends Component {
     isHome: true
   }
 
+
+
   render() {
     return (
       <React.Fragment>
       <MainNavbar isHome={this.state.isHome} />
       <div className="home">
         <div className="coverImg flex justify-center align-center">Artwork Gallery</div>
- 
-        <li ><Link className="nav-link" to='/artwork' activeClassName="active-link" exact>Image Here</Link></li>
-        <li ><Link className="nav-link" to='/artwork' activeClassName="active-link" exact>Image Here</Link></li>
-        <li ><Link className="nav-link" to='/artwork' activeClassName="active-link" exact>Image Here</Link></li>
+        <ul className="category-links clean-list">
+        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://www.howardshollow.com/canvascolor/IMAGES/modernist/b/2a.jpg"></img><span className="link-tag">Nature</span></Link></li>
+        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://i.etsystatic.com/15350345/r/il/ee7c87/1641242091/il_570xN.1641242091_iqg2.jpg"></img></Link></li>
+        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://sw16624.smartweb-static.com/upload_dir/shop/Hilma-af-klint-plakat-poster-ur.jpg"></img></Link></li>
+        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://i.etsystatic.com/15350345/r/il/ee7c87/1641242091/il_570xN.1641242091_iqg2.jpg"></img></Link></li>
+        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://sw16624.smartweb-static.com/upload_dir/shop/Hilma-af-klint-plakat-poster-ur.jpg"></img></Link></li>
+        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://www.howardshollow.com/canvascolor/IMAGES/modernist/b/2a.jpg"></img></Link></li>
+        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://sw16624.smartweb-static.com/upload_dir/shop/Hilma-af-klint-plakat-poster-ur.jpg"></img></Link></li>
+        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://www.howardshollow.com/canvascolor/IMAGES/modernist/b/2a.jpg"></img></Link></li>
+        </ul>
      </div>
      </React.Fragment>
     );
