f198e00666d60c6f49af5abdce412cacd633f7cb
unknown <ohadavidar@gmail.com>
2020-01-17 17:45:53 +0200

    bug in logged user

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import MainNavbar from '../cmps/MainNavbar';
+import { login } from '../actions/UserActions'
 
 export default class Home extends Component {
   state = {
     isHome: true
   }
 
+  componentDidMount() {
+    login({ email: "kerryjm2020@gmail.com",password: 1234567890});
+}
 
 
-  render() {
-    return (
-      <React.Fragment>
+
+render() {
+  return (
+    <React.Fragment>
       <MainNavbar isHome={this.state.isHome} />
       <div className="home">
         <div className="coverImg flex justify-center align-center">Artwork Gallery</div>
         <ul className="category-links clean-list">
-        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://www.howardshollow.com/canvascolor/IMAGES/modernist/b/2a.jpg"></img><div className="category-tag"></div></Link></li>
-        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://i.etsystatic.com/15350345/r/il/ee7c87/1641242091/il_570xN.1641242091_iqg2.jpg"></img></Link></li>
-        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://sw16624.smartweb-static.com/upload_dir/shop/Hilma-af-klint-plakat-poster-ur.jpg"></img></Link></li>
-        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://i.etsystatic.com/15350345/r/il/ee7c87/1641242091/il_570xN.1641242091_iqg2.jpg"></img></Link></li>
-        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://sw16624.smartweb-static.com/upload_dir/shop/Hilma-af-klint-plakat-poster-ur.jpg"></img></Link></li>
-        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://www.howardshollow.com/canvascolor/IMAGES/modernist/b/2a.jpg"></img></Link></li>
-        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://sw16624.smartweb-static.com/upload_dir/shop/Hilma-af-klint-plakat-poster-ur.jpg"></img></Link></li>
-        <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img  className="category-link" src="https://www.howardshollow.com/canvascolor/IMAGES/modernist/b/2a.jpg"></img></Link></li>
+          <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img className="category-link" src="https://www.howardshollow.com/canvascolor/IMAGES/modernist/b/2a.jpg"></img><div className="category-tag"></div></Link></li>
+          <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img className="category-link" src="https://i.etsystatic.com/15350345/r/il/ee7c87/1641242091/il_570xN.1641242091_iqg2.jpg"></img></Link></li>
+          <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img className="category-link" src="https://sw16624.smartweb-static.com/upload_dir/shop/Hilma-af-klint-plakat-poster-ur.jpg"></img></Link></li>
+          <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img className="category-link" src="https://i.etsystatic.com/15350345/r/il/ee7c87/1641242091/il_570xN.1641242091_iqg2.jpg"></img></Link></li>
+          <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img className="category-link" src="https://sw16624.smartweb-static.com/upload_dir/shop/Hilma-af-klint-plakat-poster-ur.jpg"></img></Link></li>
+          <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img className="category-link" src="https://www.howardshollow.com/canvascolor/IMAGES/modernist/b/2a.jpg"></img></Link></li>
+          <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img className="category-link" src="https://sw16624.smartweb-static.com/upload_dir/shop/Hilma-af-klint-plakat-poster-ur.jpg"></img></Link></li>
+          <li ><Link className="category-link" to='/artwork' activeClassName="active-link" exact><img className="category-link" src="https://www.howardshollow.com/canvascolor/IMAGES/modernist/b/2a.jpg"></img></Link></li>
         </ul>
-     </div>
-     </React.Fragment>
-    );
-  }
+      </div>
+    </React.Fragment>
+  );
+}
 }
 
 
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import ArtworkReducer from './ArtworkReducer'
-// import UserReducer from './UserReducer'
+import UserReducer from './UserReducer'
 import SystemReducer from './SystemReducer';
 
 const rootReducer = combineReducers({
   system: SystemReducer,
   artwork: ArtworkReducer,
-  // user: UserReducer
+  user: UserReducer
 })
 
 export default rootReducer;
