490715fabc5bf63ba4e8c226b19f6ef8c0beb6e1
unknown <ohadavidar@gmail.com>
2020-01-20 13:30:02 +0200

    styling

--- frontend/src/cmps/Artwork/Reviews.js
+++ frontend/src/cmps/Artwork/Reviews.js
@@ -20,15 +20,15 @@ export default class Reviews extends Component {
 
     render() {
         // console.log(this.props.reviews);
-        return <div className="comments-area">
+        return <div className="comments-container">
+             Comments:
             <form className="comment-form flex">
                 <textarea className placeholder="write something..." value={this.state.msg.txt} name="msg" onChange={this.msgHandleChange}></textarea>
                 <button onClick={(event) => this.addMsg(event, this.state.msg)}>Publish</button>
             </form>
-
-            <ul>
+            <ul className="comments-area">
                 {this.props.reviews.length > 0 && this.props.reviews.map((review, idx) => (
-                    <li key={idx}>{review.byUser.fullName}: {review.msg}</li>
+                    <li key={idx}><div className="comment-by-user">{review.byUser.fullName}</div> {review.msg}</li>
                 ))}
             </ul>
 
