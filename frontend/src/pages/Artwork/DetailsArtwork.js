3b2672eac4d6eb519d36b05f7b57614864fc6dc5
unknown <ohadavidar@gmail.com>
2020-01-20 09:38:13 +0200

    merge

--- frontend/src/pages/Artwork/DetailsArtwork.js
+++ frontend/src/pages/Artwork/DetailsArtwork.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { loadArtworkById, removeArtwork, editArtwork } from '../../actions/ArtworkActions';
+import { loadArtworkById, removeArtwork } from '../../actions/ArtworkActions';
 import { loading, doneLoading } from '../../actions/SystemActions'
 import { loadReviews,addReview } from '../../actions/ReviewActions'
 import Reviews from '../../cmps/Artwork/Reviews'
-// import Carousel from '../../cmps/Carousel';
+import Carousel from '../../cmps/Carousel';
 import Breadcrumb from '../../cmps/Breadcrumb';
 import MainNavbar from '../../cmps/MainNavbar';
 import ByArtist from '../../cmps/Artist/ByArtist';
 import Spinner from '../../cmps/Spinner';
-import like from '../../assets/images/icons/like.png';
-import Likes from '../../cmps/Artwork/Likes';
 
 class DetailsArtwork extends Component {
 
@@ -37,22 +35,9 @@ class DetailsArtwork extends Component {
         this.setState({ isAddedToCart: true })
     }
 
-    onToggleLike = async () => {
-        await this.setState(prevState => ({ isLiked: !prevState.isLiked }));
-        this.updateLiked(); 
+    onToggleLike = () => {
+        this.setState(prevState => ({ isLiked: !prevState.isLiked }));
     }
-
-    updateLiked = async () => {
-        let { currUser, selectedArtwork } = this.props;
-        let artwork = { ...selectedArtwork };
-        let usersLikes = artwork.likedByUsers;
-        this.state.isLiked ? usersLikes.push(currUser) : usersLikes.filter(user => user._id === currUser._id);
-        artwork = { ...artwork, usersLikes };
-        console.log(artwork);
-        
-        this.setState({artwork})
-        await this.props.editArtwork(artwork);
-    } 
 
     loadArtwork = async() => {
         const { _id } = this.props.match.params;
@@ -93,72 +78,32 @@ class DetailsArtwork extends Component {
         let artist;
         if (artistObj) {
             artist = artistObj.fullName;
-        }     
-        let likedByUsersObj = selectedArtwork.likedByUsers;
-        let likedByUsers;
-        if (likedByUsersObj) {
-            likedByUsers = likedByUsersObj.length;
-        }
-        console.log(likedByUsers);   
+        }        
         
         // let likes = selectedArtwork.likedByUsers.length;
         // console.log(likes);
         
         return <React.Fragment>
             <Breadcrumb />
-            <section className="details-container flex">
+            <section className="details-container flex column">
+                <div className="details-product-container flex">
                 <div className="container details-image-container">
-                    <div className="slider">
+                    
 
-                        <a href="#slide-1">1</a>
-                        <a href="#slide-2">2</a>
-                        <a href="#slide-3">3</a>
-                        <a href="#slide-4">4</a>
-                        <a href="#slide-5">5</a>
-
-                        <div className="slides">
-                            <div id="slide-1">
-                                <img src={this.props.selectedArtwork.imgUrl} ></img>
-                            </div>
-                            <div id="slide-2">
-                                2
-    </div>
-                            <div id="slide-3">
-                                3
-    </div>
-                            <div id="slide-4">
-                                4
-    </div>
-                            <div id="slide-5">
-                                5
-    </div>
-                        </div>
-                    </div>
-
-                    {/* <Carousel artSrc={selectedArtwork.imgUrl}/> */}
+                    <Carousel artSrc={selectedArtwork.imgUrl}/>
                 </div>
 
-                <div className="details-text-container">
-                    <aside className="container details-container">
+                <div className="details-text-container flex justify-space-between ">
+                    <aside className="container details-container flex justify-space-between column">
                         <ul className="aside-fill">
                             <li>
                                 <p className="art-name">{selectedArtwork.name}</p>
                                 <p className="artist-name">{artist}</p>
                                 {/* <ByArtist/> */}
-                                <div className="like-display" onClick={this.onToggleLike}>
-                                    <div className="preview-likes-container flex align-center">
-                                        <label htmlFor="like-toggle">
-                                            <img className="preview-icon-like" src={like} />
-                                        </label>
-                                        <input type="checkbox" id="like-toggle"/>
-                                        <span className="likes-counter">{likedByUsers}</span>
-                                        
-                                    </div>
-                                </div>
-                                {/* <button className="like-display" onClick={this.onToggleLike}>
+                                <button className="like-display" onClick={this.onToggleLike}>
                                     <span className={(this.state.isLiked ? 'liked-icon': 'like-icon')}></span>
                                     <span className="likes-num">13</span>
-                                </button> */}
+                                </button>
                             </li>
                             <li>
                                 <p className="art-description">{selectedArtwork.description}</p>
@@ -168,16 +113,17 @@ class DetailsArtwork extends Component {
                             </li>
                         </ul>
                         <button className="add-to-cart submit" onClick={this.addToCart}>Add To Cart</button>
+                        {this.state.isAddedToCart && <div className="purchased-modal">Purchased</div>}
                         <div className="action-btns flex justify-center">
-                            
-                            {this.state.isAddedToCart && <div className="purchased-modal">Purchased</div>}
+                        
                             <button className="btn back" onClick={this.goBack}>Back</button>
                             <Link className="btn edit-btn flex justify-center align-center" to={`/artwork/edit/${selectedArtwork._id}`}>Edit</Link>
                             <button className="btn delete" onClick={this.onDelete}></button> 
                         </div>
                     </aside>
                 </div>
-
+                </div>
+                
                 <Reviews addMsg={this.addMsg} reviews={this.props.reviews} ></Reviews>
             </section>
         </React.Fragment>
@@ -200,7 +146,6 @@ const mapDispatchToProps = {
     doneLoading,
     loadArtworkById,
     removeArtwork,
-    editArtwork,
     addReview,
     loadReviews
 }
