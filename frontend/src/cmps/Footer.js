a3dd7ed49cfc9b684edf24fbd452dfbc6e37ab74
yaelfish <winkwinksituation@gmail.com>
2020-01-23 12:21:10 +0200

    add footer

---
+++ frontend/src/cmps/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
+
+export default class Footer extends Component {
+
+    render() {
+        return (<>
+            <footer class="footer-container flex justify-center align-center">
+                <small className="container">
+                    Copyright © 2020 FineArt - All Rights Reserved to Ohad, Tal & Yael</small>
+            </footer>
+        </>)
+    }
+}
