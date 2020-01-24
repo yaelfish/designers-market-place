436a87fe5854c20e34015c44f023f8bdbfb91057
Crystalk107 <30338496+Crystalk107@users.noreply.github.com>
2020-01-24 22:35:15 +0200

    fixes issues

--- frontend/src/cmps/MainNavbar.js
+++ frontend/src/cmps/MainNavbar.js
@@ -46,55 +46,6 @@ class MainNavbar extends Component {
 
 
     render() {
-<<<<<<< HEAD
-
-        return (
-            <nav className={(this.state.pathname === "/") ? (this.state.prevScrollpos !== 0 ? "main-nav absolute scrolled" : "main-nav main-nav-home absolute") : "main-nav"}>
-                <div className="main-nav-container flex justify-space-between align-center">
-                    <ul className="nav-links flex align-center">
-                        <li>
-                            <NavLink to='/' exact>
-                                <div className="logo flex column align-center">
-                                    <img className="logo-pic" src={logo} alt="logo" />
-                                    <span className="logo-name">Early Bird</span>
-                                </div>
-
-                            </NavLink>
-                        </li>
-                        <span className="nav-separator">|</span>
-                        <li>
-                            <NavLink className="nav-link" to='/' activeClassName="active-link" exact>Home</NavLink>
-                        </li>
-                        <span className="nav-separator">|</span>
-                        <li>
-                            <NavLink className="nav-link" to='/about' activeClassName="active-link" exact>About</NavLink>
-                        </li>
-
-                        <span className="nav-separator">|</span>
-                        <li   onClick={this.loadArtworks} >
-                            <NavLink className="nav-link" to='/artwork' activeClassName="active-link" exact>Artworks</NavLink>
-                        </li>
-
-                        {this.props.loggedInUser && this.props.loggedInUser.isArtist &&
-                            <li>
-                                <span className="nav-separator">|</span>
-                                <NavLink className="nav-link" to='/artwork/add' activeClassName="active-link" exact>Add new Work</NavLink>
-                            </li>}
-                    </ul>
-
-                    {/* <div>Hello, {this.props.loggedInUser.fullName}!</div> */}
-                    {!(this.state.pathname === "/") &&
-                        <Search history={this.props.history}></Search>}
-                    <NavLink className="nav-link" to='/artist' exact>
-                        <img className="profile-pic"
-                            src={this.props.loggedInUser.imgUrl}
-                            alt={this.props.loggedInUser.userName} />
-                    </NavLink>
-                </div>
-            </nav>
-        )
-    }
-=======
     return (
         <nav className={(this.state.pathname === "/") ? (this.state.prevScrollpos !== 0 ? "main-nav absolute scrolled" : "main-nav main-nav-home absolute") : "main-nav"}>
             <div className="main-nav-container flex justify-space-between align-center">
@@ -141,7 +92,6 @@ class MainNavbar extends Component {
             </div>
         </nav>
     )}
->>>>>>> Tal
 }
 
 
