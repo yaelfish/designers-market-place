a2a5d4cbeeb61b9af7cd6fee3722fc87c4c96ea8
yaelfish <winkwinksituation@gmail.com>
2020-01-26 10:05:00 +0200

    fix ratio img inside carousel

--- frontend/src/cmps/Artwork/TemplatesArt/ArtTemplate2.js
+++ frontend/src/cmps/Artwork/TemplatesArt/ArtTemplate2.js
@@ -3,8 +3,10 @@ import wall3 from '../../../assets/images/template-imgs/wall3.jpg';
 
 export default function ArtTemplate2(props) {
 
+ 
         return (<>
-            <div className="art-template-container flex justify-center align-center">
+            <div className="art-template-container flex justify-center align-center picture-ratio">
+                {/* <img className={`artwork-small2 square-ratio ${fitImage(this)}`} */}
                 <img className="artwork-small2 square-ratio"
                     src={props.selectedArtwork} alt="artwork display on a wall"/>
                 <img className="wall-bg" src={wall3} alt="artwork display on a wall"/>
