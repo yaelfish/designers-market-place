9b071663da1942f3d1081c9714a33d7276fe2e45
unknown <ohadavidar@gmail.com>
2020-01-27 20:13:26 +0200

    please login msg

--- frontend/src/cmps/LoginModal.js
+++ frontend/src/cmps/LoginModal.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 
 export default class LoginModal extends Component {
     render() {
-        return <div className="login-modal flex justify-space-around"> You need to be logged in to perform that action 
-        <div className="close-login-modal" onClick={this.props.closeLoginModal}>X</div>
+        return <div className="login-modal flex column justify-space-between align-end"> <button className="close-login-modal delete" onClick={this.props.closeLoginModal}></button>
+            <p>You need to be logged in to perform that action</p>
         </div>
     }
 }
